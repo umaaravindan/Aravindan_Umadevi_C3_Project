c0a742d10caaa2be824f02513503a185d6c5698b
umaaravindan <106900252+umaaravindan@users.noreply.github.com>
2022-06-15 00:13:04 +0530

    Part 3: Failing test case

---
+++ Restaurant_project -Student version/src/main/java/RestaurantService.java
@@ -0,0 +1,36 @@
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RestaurantService {
+    private static List<Restaurant> restaurants = new ArrayList<>();
+
+    public Restaurant findRestaurantByName(String restaurantName){
+        if(Restaurant == null) return 1;
+		int len = Restaurant.length;
+		int i=0;
+		while (i <len)
+		{
+			if(Restaurant[i] == restaurantName) return restaurantName;
+			else i=i+1;
+		}
+		return -1;
+       }
+
+
+    public Restaurant addRestaurant(String name, String location, LocalTime openingTime, LocalTime closingTime) {
+        Restaurant newRestaurant = new Restaurant(name, location, openingTime, closingTime);
+        restaurants.add(newRestaurant);
+        return newRestaurant;
+    }
+
+    public Restaurant removeRestaurant(String restaurantName) throws restaurantNotFoundException {
+        Restaurant restaurantToBeRemoved = findRestaurantByName(restaurantName);
+        restaurants.remove(restaurantToBeRemoved);
+        return restaurantToBeRemoved;
+    }
+
+    public List<Restaurant> getRestaurants() {
+        return restaurants;
+    }
+}
